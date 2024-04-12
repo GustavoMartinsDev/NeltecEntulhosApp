1a5a3926a988d19ebdd9526eec1e21d7a0cf2b3d
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2024-04-12 00:44:39 -0300

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Pages/Header";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" index element={<Header />} />
-      <Route path="*" element={<Header />} />
-    </Routes>
-  );
+  return <Header />;
 }
 
 export default App;
